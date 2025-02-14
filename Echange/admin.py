474e582f24bcac7a290d89c36d31e3b1a354d5a9
Lucien Dilane <dilanelucien@gmail.com>
2025-02-14 11:44:49 +0100

    modif

--- Echange/admin.py
+++ Echange/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Message, Publication, Commentaire
 # Register your models here.
+
+admin.site.register(Message)
+admin.site.register(Publication)
+admin.site.register(Commentaire)
--- Groupe/models.py
+++ Groupe/models.py
@@ -6,7 +6,8 @@ from Etudiants.models import Etudiant
 class Group(models.Model):
     nom= models.CharField(max_length=50,verbose_name="nom_group",unique=True)
     description= models.TextField(max_length=300,verbose_name="description")
-    createur=models.ForeignKey(Etudiant, on_delete=models.DO_NOTHING,related_name="createur")
+    profil=models.CharField(max_length=255, verbose_name="profilGroup")
+    createur=models.ForeignKey(Etudiant, on_delete=models.SET_NULL,related_name="createur",null=True,blank=True)
     date_creation= models.DateTimeField(auto_now_add=True, editable=False,verbose_name="creationGroup")
     members= models.ManyToManyField(Etudiant, related_name="membres")
 
